85ddaa0a4cc018a427d629411eded7b03f47a7b1
probook <engsoftware1@gmail.com>
2021-03-09 12:48:04 -0300

    versao inicial

---
+++ pw-cap04/src/main/java/com/fatec/pwcap04/PwCap04Application.java
@@ -0,0 +1,13 @@
+package com.fatec.pwcap04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwCap04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwCap04Application.class, args);
+	}
+
+}
---
+++ pw-cap04/src/main/java/com/fatec/pwcap04/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.fatec.pwcap04.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public ModelAndView menu() {
+		return new ModelAndView ("paginaMenu");
+	}
+	@GetMapping("/login")
+	public ModelAndView autenticacao() {
+		return new ModelAndView ("paginaLogin");
+	}
+	@GetMapping("/aluno/cadastrar")
+	public ModelAndView cadastrarAluno() {
+		return new ModelAndView ("cadastrarAluno");
+	}
+	@GetMapping("/livro/cadastrar")
+	public ModelAndView cadastrarLivro() {
+		return new ModelAndView ("cadastrarLivro");
+	}
+}
